091c4d6261213b385bf4b7b298ce0ba78bacd136
Greg Heileman <gregheileman@gmail.com>
2018-10-28 21:06:23 -0400

    Added documentation structure

---
+++ search_index.js
@@ -0,0 +1,171 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Getting Started",
+    "title": "Getting Started",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#CurricularAnalytics-1",
+    "page": "Getting Started",
+    "title": "CurricularAnalytics",
+    "category": "section",
+    "text": "CurricularAnalytics.jl is a toolbox for studying and analyzing academic program curricula using the Julia programming language.  The toolbox represents curricula as graphs, allowing various graph-theoretic measures to be applied in order to quantify the complexity of curricula. In addition to analyzing curricular complexity, the toolbox supports the ability to visualize curricula, to compare and contrast curricula, to create optimal degree plans for completing curricula that satisfy particular constraints, and to simulate the impact of various events on student progression through a curriculum. "
+},
+
+{
+    "location": "#Basic-toolbox-functionality-1",
+    "page": "Getting Started",
+    "title": "Basic toolbox functionality",
+    "category": "section",
+    "text": "The basic data types used in the CurricularAnalytics.jl libraries are described in CurricularAnalytics Data Types. The toolbox includes a number of convenient functions that can be used to create a curriculum graph; these are described in Creating Curricula. In addition, functions that can be used to create a degree plan from a curriculum are described in [Creating Degree Plans]. Detailed example and tutorials on how to create and analyze curricula and degree plans can be found in the CurricularAnalytics Tutorial Notebooks repository."
+},
+
+{
+    "location": "types/#",
+    "page": "CurricularAnalytics Data Types",
+    "title": "CurricularAnalytics Data Types",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "types/#CurricularAnalytics-Data-Types-1",
+    "page": "CurricularAnalytics Data Types",
+    "title": "CurricularAnalytics Data Types",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "curriculum/#",
+    "page": "Creating Curricula",
+    "title": "Creating Curricula",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "curriculum/#Creating-Curricula-1",
+    "page": "Creating Curricula",
+    "title": "Creating Curricula",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "degreeplan/#",
+    "page": "Creating Degree Plans",
+    "title": "Creating Degree Plans",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "degreeplan/#Creating-Degree-Plans-1",
+    "page": "Creating Degree Plans",
+    "title": "Creating Degree Plans",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "persistence/#",
+    "page": "Reading/Writing Curricula & Degree Plans",
+    "title": "Reading/Writing Curricula & Degree Plans",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "persistence/#Reading/Writing-Curricula-\\-and-Degree-Plans-1",
+    "page": "Reading/Writing Curricula & Degree Plans",
+    "title": "Reading/Writing Curricula \\& Degree Plans",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "plotting/#",
+    "page": "Visualizing Degree Plans",
+    "title": "Visualizing Degree Plans",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "plotting/#Visualizing-Degree-Plans-1",
+    "page": "Visualizing Degree Plans",
+    "title": "Visualizing Degree Plans",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "metrics/#",
+    "page": "Curricular Metrics",
+    "title": "Curricular Metrics",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "metrics/#Curricular-Metrics-1",
+    "page": "Curricular Metrics",
+    "title": "Curricular Metrics",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "optimizing/#",
+    "page": "Optimizing Degree Plans",
+    "title": "Optimizing Degree Plans",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "optimizing/#Optimizing-Degree-Plans-1",
+    "page": "Optimizing Degree Plans",
+    "title": "Optimizing Degree Plans",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "simulating/#",
+    "page": "Simulating Student Flows",
+    "title": "Simulating Student Flows",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "simulating/#Simulating-Student-Flows-1",
+    "page": "Simulating Student Flows",
+    "title": "Simulating Student Flows",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "developing/#",
+    "page": "Developer Notes",
+    "title": "Developer Notes",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "developing/#Developer-Notes-1",
+    "page": "Developer Notes",
+    "title": "Developer Notes",
+    "category": "section",
+    "text": ""
+},
+
+]}
