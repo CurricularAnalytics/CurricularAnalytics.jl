9e7a1510da3eab1b5a1fed77ce92a3f6044b7801
Hayden Free <hayden.free@uky.edu>
2018-10-30 13:48:39 -0400

    Remove unnecessary include statement, move other to top

--- src/CurricularAnalytics.jl
+++ src/CurricularAnalytics.jl
@@ -10,6 +10,7 @@ include("GraphAlgs.jl")
 include("JsonHandler.jl")
 include("Visualization.jl")
 include("Optimization.jl")
+include("DegreePlanAnalytics.jl")
 
 export Degree, AA, AS, AAS, BA, BS, System, semester, quarter, Requisite, pre, co, strict_co,
         EdgeClass, LearningOutcome, Course, add_requisite!, delete_requisite!, Curriculum, 
@@ -266,8 +267,5 @@ function compare_curricula(c1::Curriculum, c2::Curriculum)
     end
     return report
 end
-
-include("./DegreePlanAnalytics.jl")
-include("./Visualization.jl")
 
 end # module
