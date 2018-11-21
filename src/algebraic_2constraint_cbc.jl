9c6d0c57428e8f738f20b48b187f7b07d8957b2e
Will Thompson <32341429+wgthompson@users.noreply.github.com>
2018-11-21 11:12:58 -0500

    Create algebraic_2constraint_cbc.jl

---
+++ src/algebraic_2constraint_cbc.jl
@@ -0,0 +1,34 @@
+using JuMP
+using Cbc
+
+# Preparing an optimization model
+m = Model(solver=CbcSolver())
+
+# Declaring variables
+@variable(m, 0<= x1 <=10)
+@variable(m, x2 >=0)
+@variable(m, x3 >=0)
+
+# Setting the objective
+@objective(m, Max, x1 + 2x2 + 5x3)
+
+# Adding constraints
+@constraint(m, constraint1, -x1 +  x2 + 3x3 <= -5)
+@constraint(m, constraint2,  x1 + 3x2 - 7x3 <= 10)
+
+# Printing the prepared optimization model
+print(m)
+
+# Solving the optimization problem
+solve(m)
+
+# Printing the optimal solutions obtained
+println("Optimal Solutions:")
+println("x1 = ", getvalue(x1))
+println("x2 = ", getvalue(x2))
+println("x3 = ", getvalue(x3))
+
+# Printing the optimal dual variables
+println("Dual Variables:")
+println("dual1 = ", getdual(constraint1))
+println("dual2 = ", getdual(constraint2))
