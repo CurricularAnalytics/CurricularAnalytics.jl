fc13920e84c9a653c13b5407872c9de11f86387d
Hayden Free <hayden.free@uky.edu>
2019-07-15 10:51:03 -0400

    remove comments from API.jl

--- src/API.jl
+++ src/API.jl
@@ -1,21 +1,8 @@
-# https://discourse.julialang.org/t/write-a-rest-interface-like-flask/18538/16
-
-# Bukdu v0.4.1
 using Bukdu
 
 struct RESTController <: ApplicationController
     conn::Conn
 end
-
-# function create(c::RESTController)
-#     @info :payload (c.params.message, c.params.x, c.params.y)
-#     render(JSON, "OK")
-# end
-
-# routes() do
-#     post("/messages", RESTController, create)
-#     plug(Plug.Parsers, parsers=[:json])
-# end
 
 function validate(c::RESTController)
     @info :PAYLOAD (c.params.curriculum)
