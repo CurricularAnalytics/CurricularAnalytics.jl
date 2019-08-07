d577595721ba3a42c161d515c7829c1e12cc12e1
Greg Heileman <gregheileman@gmail.com>
2019-08-06 17:32:02 -0700

    added a notebook, and requisite_distance functions

--- src/CurricularAnalytics.jl
+++ src/CurricularAnalytics.jl
@@ -25,8 +25,9 @@ include("Visualization.jl")
 
 export Degree, AA, AS, AAS, BA, BS, System, semester, quarter, Requisite, pre, co, strict_co,
         EdgeClass, LearningOutcome, Course, add_requisite!, delete_requisite!, Curriculum, 
-        total_credits, requisite_type, Term, DegreePlan, dfs, longest_path, long_paths,
-        isvalid_curriculum, extraneous_requisites, blocking_factor, delay_factor, centrality,
+        total_credits, requisite_type, Term, DegreePlan, find_term, course_from_id, requisite_distance, 
+        dfs, longest_path, 
+        long_paths, isvalid_curriculum, extraneous_requisites, blocking_factor, delay_factor, centrality,
         complexity, compare_curricula, isvalid_degree_plan, print_plan, visualize, basic_metrics,
         read_csv, create_degree_plan, bin_packing, bin_packing2, find_min_terms, add_lo_requisite!, 
         update_plan, write_csv, find_min_terms, balance_terms, balance_terms_opt, find_min_terms_opt, 
