2f0034f6b6fb032d7023e74ad6c14ccec57a8d37
Greg Heileman <gregheileman@gmail.com>
2020-02-15 09:12:54 -0700

    added tests: all functions in DegreePlanAnalytics.jl

--- test/DegreePlanAnalytics.jl
+++ test/DegreePlanAnalytics.jl
@@ -1,29 +1,40 @@
 # DegreePlanAnalytics tests
 
-@testset "DegreePlanAnalytics Tests" begin
+# 6-vertex test curriculum - valid
+#       
+#      /-------------------*\
+#    A --------* C           E \*         
+#              /---------*/ */  |
+#             /  |----------/  /
+#    B-------/   D            F           
+#     \---------------------*/
 #
+#    (A,C) - pre;  (A,E) -  pre; (B,E) - pre; (B,F) - pre; (D,E) - co; (F,E) - strict_co
+
+@testset "DegreePlanAnalytics Tests" begin
+
 A = Course("A", 3)
 B = Course("B", 1)
-C = Course("C", 3)
+C = Course("C", 2)
 D = Course("D", 1)
-E = Course("E", 3)
-F = Course("F", 3)
+E = Course("E", 4)
+F = Course("F", 1)
 
 add_requisite!(A,E,pre)
 add_requisite!(A,C,pre)
 add_requisite!(B,E,pre)
 add_requisite!(B,F,pre)
-add_requisite!(D,E,pre)
-add_requisite!(E,E,strict_co)
+add_requisite!(D,E,co)
+add_requisite!(F,E,strict_co)
 
-curric = Curriculum("Test Curric", [A,B,C,D,E,F], sortby_ID=false)
+curric = Curriculum("Test Curric", [A,B,C,D,E,F])
 terms = Array{Term}(undef, 3)
 terms[1] = Term([A,B])
 terms[2] = Term([C,D])
 terms[3] = Term([E,F])
 dp = DegreePlan("Test Plan", curric, terms)
 
-# Test requisite_distance(course) 
+# Test requisite_distance(plan, course) and requisite_distance(plan)
 @test requisite_distance(dp, A) == 0
 @test requisite_distance(dp, B) == 0
 @test requisite_distance(dp, C) == 1
@@ -31,4 +42,16 @@ dp = DegreePlan("Test Plan", curric, terms)
 @test requisite_distance(dp, E) == 5
 @test requisite_distance(dp, F) == 2
 @test requisite_distance(dp) == 8
+
+# Test basic basic_metrics(plan)
+basic_metrics(dp)
+@test dp.metrics["total credit hours"] == 12
+@test dp.metrics["avg. credits per term"] == 4.0
+@test dp.metrics["min. credits in a term"] == 3
+@test dp.metrics["term credit hour std. dev."] ≈ 0.816497 atol=1e-5
+@test dp.metrics["number of terms"] == 3
+@test dp.metrics["max. credits in a term"] == 5
+@test dp.metrics["min. credit term"] == 2
+@test dp.metrics["max. credit term"] == 3
+
 end;
